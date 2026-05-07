687f35df811d691b1be1ccd5c8ce79fa99436251
aurly1717 <aurly_zacci_wardani@teknokrat.ac.id>
2026-05-07 13:05:22 +0700

    aurly: menambahkan logo navbar

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -6,7 +6,17 @@ export default function Navbar() {
   return (
     <header className="sticky top-0 z-50 w-full">
       <nav className="container mx-auto flex items-center justify-between px-4 py-4">
-        Navbar
+
+        <div className="flex items-center gap-3 text-white">
+          <span className="text-2xl font-semibold">
+            Kyokko Beach
+          </span>
+
+          <span className="text-xl">
+            🌴
+          </span>
+        </div>
+
       </nav>
     </header>
   );
