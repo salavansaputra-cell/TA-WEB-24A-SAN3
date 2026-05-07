523279a9e297578db937613de8c1a361c4d952b2
aurly1717 <aurly_zacci_wardani@teknokrat.ac.id>
2026-05-07 13:25:27 +0700

    aurly: menambahkan section about pada footer

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -1,9 +1,21 @@
 export default function Footer() {
   return (
     <footer className="bg-slate-950 text-slate-100">
+
       <div className="container mx-auto px-4 py-12">
-        Footer
+
+        <div>
+          <h2 className="text-lg font-semibold text-white">
+            About
+          </h2>
+
+          <p className="mt-4 max-w-md text-sm leading-7 text-slate-300">
+            Kyokko Beach is a scenic gateway to Lampung’s stunning coastline.
+          </p>
+        </div>
+
       </div>
+
     </footer>
   );
 }
