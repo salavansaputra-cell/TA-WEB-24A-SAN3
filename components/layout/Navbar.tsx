bf6b9402198499f688a16107562ba88bd1b05928
aurly1717 <aurly_zacci_wardani@teknokrat.ac.id>
2026-05-07 13:04:03 +0700

    aurly: menambahkan layout dan import navbar

---
+++ components/layout/Navbar.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import Link from "next/link";
+
+export default function Navbar() {
+  return (
+    <header className="sticky top-0 z-50 w-full">
+      <nav className="container mx-auto flex items-center justify-between px-4 py-4">
+        Navbar
+      </nav>
+    </header>
+  );
+}
