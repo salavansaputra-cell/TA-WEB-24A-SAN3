5026905ac361f3517e5533be78975d533577a746
aurly1717 <aurly_zacci_wardani@teknokrat.ac.id>
2026-05-07 13:24:48 +0700

    aurly: menambahkan layout footer

---
+++ components/layout/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="bg-slate-950 text-slate-100">
+      <div className="container mx-auto px-4 py-12">
+        Footer
+      </div>
+    </footer>
+  );
+}
