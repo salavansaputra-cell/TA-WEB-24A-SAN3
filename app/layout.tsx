a2eb31aca5aa02e548400c4e6d08c09bd3975131
aurly1717 <aurly_zacci_wardani@teknokrat.ac.id>
2026-05-07 13:39:34 +0700

    aurly: menambahkan styling body pada layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,9 +25,9 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable}`}
+      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body>
+      <body className="min-h-full flex flex-col">
         {children}
       </body>
     </html>
