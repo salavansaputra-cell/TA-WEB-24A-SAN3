1d8cf108aed65bb48463009ae23589fad9289376
salavanalifsaputra <salavan_alif_saputra@teknokrat.ac.id>
2026-05-08 17:41:46 +0700

    Salavan:Membuat promo banner

---
+++ components/info/PromoBanner.tsx
@@ -0,0 +1,7 @@
+export default function PromoBanner() {
+  return (
+    <div className="bg-yellow-400 text-yellow-900 p-4 text-center">
+      <p className="font-semibold">Special Promo: 20% off for weekend bookings!</p>
+    </div>
+  )
+}
